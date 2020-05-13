da15634c350e878c7e87ad99485e8b29caaaf64c
Nicholas Tran <tran.nickk@gmail.com>
2020-05-13 15:54:55 -0400

    Added Scrollspy functionality to keep track of scroll position.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,15 @@
-/*!
-    * Start Bootstrap - Grayscale v6.0.0 (https://startbootstrap.com/themes/grayscale)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-grayscale/blob/master/LICENSE)
-    */
-    // Collapse Navbar
-    var navbarCollapse = function () {
-        if ($("#mainNav").offset().top > 100) {
-            $("#mainNav").addClass("smallNav");
-        } else {
-            $("#mainNav").removeClass("smallNav");
-        }
-    };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
+// Collapse Navbar if scrolled past a certain amount.
+var collapseNav = function () {
+  if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("smallNav");
+  } else {
+      $("#mainNav").removeClass("smallNav");
+  }
+};
+// Collapse now if page is not at top
+collapseNav();
+// Collapse the navbar when page is scrolled
+$(window).scroll(collapseNav);
+
+//Use scrollspy to highlight nav links when active
+$("body").scrollspy({target: "#mainNav"})
