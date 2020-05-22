7015bf45e91ee2a07bed713b787a82dc7aa15989
Nicholas Tran <tran.nickk@gmail.com>
2020-05-22 00:01:20 -0400

    Removed anchor tags from url when clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,8 @@
-$(function () {
-    $('a[href*="#"]:not([href="#"])').click(function () {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html, body').animate({
-                    scrollTop: target.offset().top
-                }, 1000);
-                return false;
-            }
-        }
-    });
+//Remove anchor tag from url when anchor link clicked
+window.addEventListener("hashchange", function(event){
+    var noHashURL = window.location.href.replace(/#.*$/, '');
+    window.history.replaceState('', document.title, noHashURL);
 });
-
 //Collapse navbar when scroll link is pressed
 $(".scroll-trigger").click(function() {
   $(".navbar-collapse").collapse("hide");
