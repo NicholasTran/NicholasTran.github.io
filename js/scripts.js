a0d877f0ec51241f1152be0821263faae34a8ade
Nicholas Tran <tran.nickk@gmail.com>
2020-05-21 22:45:19 -0400

    Testing

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
+$(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+//Collapse navbar when scroll link is pressed
+$(".scroll-trigger").click(function() {
+  $(".navbar-collapse").collapse("hide");
+});
+
+//Use scrollspy to highlight nav links when active
+$("body").scrollspy({target: "#mainNav"});
 
 // Collapse Navbar if scrolled past a certain amount.
 var collapseNav = function() {
@@ -11,12 +33,3 @@ var collapseNav = function() {
 collapseNav();
 // Collapse the navbar when page is scrolled
 $(window).scroll(collapseNav);
-
-//Use scrollspy to highlight nav links when active
-$("body").scrollspy({target: "#mainNav"});
-
-
-//Collapse navbar when scroll link is pressed
-$(".scroll-trigger").click(function() {
-  $(".navbar-collapse").collapse("hide");
-});
