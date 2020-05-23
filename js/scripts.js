8f07d5029fbf660aa9a5473920cae91af5720276
Nicholas Tran <tran.nickk@gmail.com>
2020-05-23 15:08:29 -0400

    Disabled hover effects on scroll to prevent stutter.

--- js/scripts.js
+++ js/scripts.js
@@ -40,3 +40,15 @@ var collapseNav = function() {
 collapseNav();
 // Collapse the navbar when page is scrolled
 $(window).scroll(collapseNav);
+
+//Disable hover effects on scroll
+var body = document.body,timer;
+window.addEventListener('scroll', function() {
+   clearTimeout(timer);
+  if(!body.classList.contains('disable-hover')) {
+    body.classList.add('disable-hover')
+  }
+   timer = setTimeout(function(){
+    body.classList.remove('disable-hover')
+  },100);
+}, false);
